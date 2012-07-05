38c4e7d4268669914ee5f7417b0e1761b8d5f6a0
He-Hsiung Wang <munouzin@gmail.com>
2012-07-06 00:15:56 +0800

    plugin information

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :pervoka_achievement do
   name 'Pervoka Achievement plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'munouzin'
+  description 'An configurable achievement system for redmine, a fantastic project management web application.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/bonau/PervokaAchievement'
+  author_url 'https://github.com/bonau'
 end
