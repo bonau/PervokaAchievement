da2a072c6f75003c3ce4a9272da828bf4a206645
He-Hsiung Wang <munouzin@gmail.com>
2013-03-15 13:14:25 +0800

    add some flavor to mailer template
    - rename mail to achievement_unlocked
    - correct the mailer instance methods

--- lib/pervoka_achievement/mailer_patch.rb
+++ lib/pervoka_achievement/mailer_patch.rb
@@ -10,14 +10,15 @@ module PervokaAchievement
       end
 
       module ClassMethods
+      end
+      module InstanceMethods
         def achievement_unlocked(achievement)
           user = achievement.user
           set_language_if_valid user.language
+          @achievement = l(achievement.class.locale_prefix)
           mail :to => user.mail,
             :subject => l(:"achievement.unlocked", :title => l(achievement.class.locale_prefix(:title)))
         end
-      end
-      module InstanceMethods
       end
     end
   end
