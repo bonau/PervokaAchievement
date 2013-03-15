7ed90d26e44f56f738db53a5ee20cc31ba86059b
He-Hsiung Wang <munouzin@gmail.com>
2013-03-15 14:30:24 +0800

    cleanup for unused file/codes

--- app/models/issue_observer.rb
+++
@@ -1,2 +0,0 @@
-class IssueObserver < ActiveRecord::Observer
-end
--- test/unit/achievement_test.rb
+++ test/unit/achievement_test.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class AchievementTest < ActiveSupport::TestCase
-
-  # Replace this with your real tests.
   def user_should_not_be_nil
     achievement = Achievement.new
     assert !achievement.save
