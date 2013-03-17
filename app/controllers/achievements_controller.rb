aa73056a88017a7de6e574387fff50044e6fb7b6
He-Hsiung Wang <munouzin@gmail.com>
2013-03-18 00:40:00 +0800

    now the achievement list view shows the unlockable achievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -4,5 +4,7 @@ class AchievementsController < ApplicationController
   def index
     @all_achievement_classes = Achievement.registered_achievements
     @user_achievements = User.current.achievements
+    @unlocked_achievement_classes = @user_achievements.map{ |a| a.class }.uniq
+    @unlockable_achievement_classes = @all_achievement_classes - @unlocked_achievement_classes
   end
 end
